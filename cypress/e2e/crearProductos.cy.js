f343f58dd7f23430811d508ce8728ad5c2739811
m4t124 <matias.morales.02@alu.ucm.cl>
2024-11-24 18:32:02 -0300

    cambios correos Cypress

--- cypress/e2e/crearProductos.cy.js
+++ cypress/e2e/crearProductos.cy.js
@@ -8,12 +8,12 @@ describe('Crear Productos', () => {
         cy.visit('http://localhost:3001/login-emprendedor')
 
         cy.get('input[name="correo"]')
-        .type('matiasnmv20@gmail.com')
-        .should('have.value', 'matiasnmv20@gmail.com');
+        .type('todopaldrive@gmail.com')
+        .should('have.value', 'todopaldrive@gmail.com');
   
         cy.get('input[name="contrasena"]')
-        .type('alergico2024')
-        .should('have.value', 'alergico2024');
+        .type('pantera')
+        .should('have.value', 'pantera');
   
         cy.get('button.login-emprendedor-button')
         .click();
@@ -22,10 +22,10 @@ describe('Crear Productos', () => {
 
         cy.visit('http://localhost:3001/gestionProducto')
 
-        cy.get('#nombre_producto').type('Caballo de mimbre');
-        cy.get('#precio_producto').type('3000');
-        cy.get('#descripcion_producto').type('Caballo de mimbre para decorar la casa');
-        cy.get('#id_categoria').select('3');
+        cy.get('#nombre_producto').type('Chaleco para perros galgos');
+        cy.get('#precio_producto').type('5000');
+        cy.get('#descripcion_producto').type('Chaleco para las temporadas de invierno');
+        cy.get('#id_categoria').select('2');
         cy.get('#cantidad_disponible').type('30');
 
         cy.get('#imagen').attachFile('productoCM.jpg');
