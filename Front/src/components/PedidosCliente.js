7a52aa96cbd422a240dcc2684399d0715eaea506
Jose Sepulveda <jose.sepulveda.03@alu.ucm.cl>
2024-11-29 14:56:48 -0300

    mejorando pedido clientes

--- Front/src/components/PedidosCliente.js
+++ Front/src/components/PedidosCliente.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { AuthContext } from '../Auth/AuthContext';
 import { getPedidoByCliente } from '../services/pedidos';
+import "../Styles/pedidos-cliente.css";
 
 const PedidosCliente = () => {
     const [pedidos, setPedidos] = useState([]);
@@ -14,7 +15,8 @@ const PedidosCliente = () => {
             }
             try {
                 const response = await getPedidoByCliente(auth.id);
-                setPedidos(response.data);   
+                const listPedidos = response.data.sort ((a,b) => a.id_pedido - b.id_pedido)
+                setPedidos(listPedidos);   
             } catch (error) {
                 console.error('Error al cargar los pedidos', error);
                 toast.error('Error al cargar los pedidos')
@@ -24,13 +26,17 @@ const PedidosCliente = () => {
         cargarPedidos();
     }, [auth.id]);
 
+    const generarUrl = (fileId) => {
+        return `https://drive.google.com/thumbnail?id=${fileId}&sz=w1000`;
+    }
+
     return (
-        <div className='gestion-pedidos'>
+        <div className='pedidos-cliente'>
             <h1 className='titulo'>Mis Pedidos</h1>
             {pedidos && pedidos.length > 0 ? (
                 pedidos.map((pedido) => (
-                    <div key={pedido.id_pedido} className='pedido-container'> 
-                    <table className='tabla-pedido'>
+                    <div key={pedido.id_pedido} className='pedido-cliente-container'> 
+                    <table className='tabla-pedidos-cliente'>
                         <thead>
                             <tr>
                                 <th>ID</th>
@@ -51,24 +57,34 @@ const PedidosCliente = () => {
                                 <td>{new Date(pedido.venta.fecha_venta).toLocaleDateString('es-CL')}</td>
                             </tr>
                             <tr>
-                                <td>Subtotal</td>
-                                <td>{pedido.venta.subtotal}</td>
-                            </tr>
-                            <tr>
-                                <td>Decuentos</td>
-                                <td>{pedido.venta.descuentos}</td>
-                            </tr>
-                            <tr>
-                                <td>IVA</td>
-                                <td>{pedido.venta.iva}</td>
-                            </tr>
-                            <tr>
                                 <td>Total</td>
                                 <td>{pedido.venta.total}</td>
                             </tr>
                             <tr>
                                 <td>Metodo de pago</td>
                                 <td>{pedido.venta.metodo_de_pago}</td>
+                            </tr>
+                            <tr className='subtitulo'>
+                                <td>Productos</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Nombre</td>
+                                <td>{pedido.producto.nombre_producto}</td>
+                            </tr>
+                            <tr>
+                                <td>Descripción</td>
+                                <td>{pedido.producto.descripcion_producto}</td>
+                            </tr>
+                            <tr>
+                                <td>Precio</td>
+                                <td>${pedido.producto.precio_producto}</td>
+                            </tr>
+                            <tr>
+                                <td>Imagen</td>
+                                <td>
+                                    <img alt='' src={generarUrl(pedido.producto.imagen)}/>
+                                </td>
                             </tr>
                         </tbody>
                     </table>  
